20666be258ba3069c9445afa556a173e15c0366d
Dipak Bankar <110369016+Dipakbank@users.noreply.github.com>
2023-02-11 17:56:55 +0530

    Add files via upload

---
+++ Tut19.cpp
@@ -0,0 +1,69 @@
+//Function overloading 
+
+# include <iostream> 
+using namespace std;
+
+int sum(int a,int b)
+{   cout<<"Sum of 2 int is :"<<endl;
+    return a +b ;
+
+}
+
+int sum(int a,int b, int c)
+{   cout<<"Sum of 3 int is :"<<endl;
+    return a +b +c;
+}
+//***********************************************
+float volume(float a)
+{
+    cout<<"Volume of square is :"<<endl;
+    return (a*a*a);
+}
+
+float volume(float a, float b, float c)
+{
+    cout<<"Volume of rectangle is :"<<endl;
+    return (a*b*c);
+}
+
+float volume(float r, float h)
+{
+    cout<<"Volume of cylinder is :"<<endl;
+    return (3.14*r*r*h);
+}
+
+int main()
+{
+    cout<<sum(3, 4)<<endl;
+    cout<<endl;
+    cout<<sum(3, 4, 50)<<endl;
+    cout<<endl;
+
+    cout<<"The valume of square is"<<endl<<volume(2)<<endl;
+    cout<<endl;
+    cout<<"The valume of cylinder is"<<endl<<volume(2,5)<<endl;
+    cout<<endl;
+    cout<<"The valume of rectangle is"<<endl<<volume(2,5,4)<<endl;
+    
+
+
+}
+/*
+Sum of 2 int is :        
+7
+
+Sum of 3 int is :        
+57
+
+Volume of square is :    
+The valume of square is  
+8
+
+Volume of cylinder is :  
+The valume of cylinder is
+62.8
+
+Volume of rectangle is : 
+The valume of rectangle is
+40
+*/
